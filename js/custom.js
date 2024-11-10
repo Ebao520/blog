bdf0daae15bee473004042f2ca6a58991330356a
jerry pan <530127862@qq.com>
2024-11-10 17:21:23 +0800

    Site updated: 2024-11-10 17:21:19

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function (e) {
-    $('.copyright').html('©2024 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> E宝办公');
+    $('.copyright').html('Copyright 2024 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> 版权所有：E宝办公助手');
 });
 
 $(document).ready(function (e) {
@@ -25,16 +25,16 @@ function show_date_time() {
     var seconds = Math.floor((e_minsold - minsold) * 60);
     // 设置所有时间单位和文字为黑色
     document.getElementById('span_dt_dt').innerHTML =
-        '<span style="color: black;">' + daysold + '</span> <span style="color: black;">天</span> ' +
-        '<span style="color: black;">' + hrsold + '</span> <span style="color: black;">时</span> ' +
-        '<span style="color: black;">' + minsold + '</span> <span style="color: black;">分</span> ' +
-        '<span style="color: black;">' + seconds + '</span> <span style="color: black;">秒</span>';
+        '<span style="color: #808080;">' + daysold + '</span> <span style="color: #808080;">天</span> ' +
+        '<span style="color: #808080;">' + hrsold + '</span> <span style="color: #808080;">时</span> ' +
+        '<span style="color: #808080;">' + minsold + '</span> <span style="color: #808080;">分</span> ' +
+        '<span style="color: #808080;">' + seconds + '</span> <span style="color: #808080;">秒</span>';
 
 }
 // source/js/custom.js
 document.addEventListener('DOMContentLoaded', function () {
     var options = {
-        strings: ["一款佛系的Excel插件", "轻松处理您的数据", "提升工作效率", "简单、易用、高效的Excel插件", "在数据的海洋中航行", "让数字在表格中舞蹈飞扬", "从枯燥的行列中解放", "让繁琐的工作变得轻松", "在每一个单元格中创造奇迹。"],
+        strings: ["一款佛系的Excel工具箱软件", "轻松处理您的数据", "提升工作效率", "简单、易用、高效的办公助手", "在数据的海洋中航行", "让数字在表格中舞蹈飞扬", "从枯燥的行列中解放", "让繁琐的工作变得轻松", "在每一个单元格中创造奇迹。"],
         typeSpeed: 80,  // Typing speed in milliseconds
         backSpeed: 40,  // Backspacing speed in milliseconds
         loop: true      // Loop the typing animation
